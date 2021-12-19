58baf6d43b2ed8ddd4e4e668be0693ac8b894a21
JoaoTony <joaojosetony2016@gmail.com>
2021-12-19 23:41:52 +0100

    feix: notification message

--- components/navbar/navbar.notifications.tsx
+++ components/navbar/navbar.notifications.tsx
@@ -2,16 +2,21 @@ import { FC } from 'react'
 
 import { INotification } from './navbar.types'
 import { Notifications as Container } from './navbar.styles'
-import { typeCodeToText } from './navbar.utils'
+import { customUseContext } from '../../context'
+import { typeCodeToText, formatedName } from './navbar.utils'
 
-const Notifications: FC<{notifications: Array<INotification>}> = ({ notifications }) => (
+const Notifications: FC<{notifications: Array<INotification>}> = ({ notifications }) => {
+  const { activeUser } = customUseContext()
+
+  return (
   <Container>
     <div className="arrow"/>
     {notifications.map((notification, index) =>
       (<p key={index.toString()} className="notification">
-        {notification.senderName} {typeCodeToText(notification.type)} yor photo
+        {formatedName(activeUser.name, notification.senderName)} {typeCodeToText(notification.type)} your photo
     </p>))}
   </Container>
-)
+  )
+}
 
 export default Notifications
--- components/navbar/navbar.utils.ts
+++ components/navbar/navbar.utils.ts
@@ -1,3 +1,5 @@
 const typeCodes = ['Liked', 'commented', 'shared']
 
 export const typeCodeToText = (type: number) => typeCodes[type]
+
+export const formatedName = (avtive: string, name: string) => name === avtive ? 'You' : name
